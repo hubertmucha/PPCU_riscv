87ffc74427e50be1714dec07344a3fdc9605f893
hubertmucha <hub.muc@wp.pl>
2022-05-24 20:44:20 +0200

    C program

--- sw/app/src/main.cpp
+++ sw/app/src/main.cpp
@@ -1,14 +1,31 @@
+
 #include "delay.h"
 #include "gpio.h"
 
-static constexpr uint32_t led_mask{0xf};
+//static constexpr uint32_t led_mask{0xf};
+
+#define TAB_SIZE 50
+
+
+// SOS
+static uint8_t tab0[TAB_SIZE] = {1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+// Hubert
+static uint8_t tab1[TAB_SIZE] = {1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0};
+// Mucha
+static uint8_t tab2[TAB_SIZE] = {1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0};
+
 
 int main()
 {
     while (true) {
-        for (int i = 0; i < 16; ++i) {
-            gpio.set_mask_bits(led_mask, i);
+        gpio.set_pin(0, 1);
+        for (uint8_t i = 0; i < TAB_SIZE; ++i) { 
+            gpio.set_pin(1, tab0[i]);
+            gpio.set_pin(2, tab1[i]);
+            gpio.set_pin(3, tab2[i]);
             mdelay(1000);
         }
+        gpio.set_pin(0, 0);
+        mdelay(1000);
     }
 }
